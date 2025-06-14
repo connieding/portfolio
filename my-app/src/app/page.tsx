a4774381fad1bd7e9f012e71b41c070886e86fd3
Connie Ding <cdin152@aucklanduni.ac.nz>
2025-06-14 23:30:49 +1200

    chore: initial setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Card from "./components/Card";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <Card />
+    </div>
+  );
+}
